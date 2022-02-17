d6b883fe2e63540d730b851ddee848a4e16c87b6
Auntor Acharja <acharjeeauntor@gmail.com>
2022-02-17 22:48:22 +0600

    Complete DropDown Feature Automation

--- pages/dropdown.page.js
+++ pages/dropdown.page.js
@@ -1,18 +1,63 @@
-const dropdownPageSelectors={
-    fruitsDropdownSelector:"#fruits"
+const dropdownPageSelectors = {
+    fruitsDropdownSelector: "#fruits",
+    selectedOptionTextSelector: ".content .subtitle",
+    heroDropDownSelector: "#superheros",
+    languageDropdownSelector: "#lang",
+    languageOptionSelector:"#lang option",
+    countryOptionSelector:"#country"
 }
 
-class DropdownPage{
-    constructor(page){
+class DropdownPage {
+    constructor(page) {
         this.page = page
     }
 
-async selectFruit(text){
-    const ele = await this.page.locator(dropdownPageSelectors.fruitsDropdownSelector)
-    await ele.selectOption(text)
+    async selectFruit(text) {
+        const ele = await this.page.locator(dropdownPageSelectors.fruitsDropdownSelector)
+        await ele.selectOption({ label: text })
+    }
+
+    async getSelectedFruitName() {
+        const ele = await this.page.locator(dropdownPageSelectors.selectedOptionTextSelector)
+        return ele.textContent()
+    }
+
+    async selectSuperHero(label, value, index) {
+        const ele = await this.page.locator(dropdownPageSelectors.heroDropDownSelector)
+        await ele.selectOption([{ label: label }, { value: value }, { index: index }])
+    }
+
+    async getSelectedHeroName() {
+        const ele = await this.page.locator(dropdownPageSelectors.selectedOptionTextSelector).nth(1)
+        return ele.textContent()
+    }
+
+    async getAllLangOptions() {
+     // const lang = await this.page.locator(dropdownPageSelectors.languageOptionSelector)
+      const lang = await this.page.$$(dropdownPageSelectors.languageOptionSelector)
+      const len =await lang.length
+      for(var i=0;i<len;i++){
+        console.log(await this.page.locator(dropdownPageSelectors.languageOptionSelector).nth(i).textContent())
+        if(i===len-1){
+            const ele = await this.page.locator(dropdownPageSelectors.languageDropdownSelector)
+            await ele.selectOption({index:len-1})
+            return this.page.locator(dropdownPageSelectors.languageOptionSelector).nth(len-1).textContent()
+        }
+      }
+    }
+
+    async getSelectedProgrammingLanguage() {
+        const ele = await this.page.locator(dropdownPageSelectors.selectedOptionTextSelector).nth(1)
+        return ele.textContent()
+    }
+
+    async selectCounty(countryName) {
+        await this.page.selectOption(dropdownPageSelectors.countryOptionSelector,countryName)
+        const val = await this.page.$eval(dropdownPageSelectors.countryOptionSelector,el=> el.value)
+        console.log(val)
+        return val
+    }
+
 }
 
-
-}
-
-module.exports={DropdownPage,dropdownPageSelectors}
+module.exports = { DropdownPage, dropdownPageSelectors }
--- tests/dropdown.test.js
+++ tests/dropdown.test.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test')
 const { TestPage } = require("../pages/test.page")
 const { Common } = require("../helper/common")
 const { DropdownPage, alertPageSelectors } = require("../pages/dropdown.page")
+const DropDownData = require("../projectVariables/dropdownData.json")
 
 let dropdownPage, testPage, page, common
 
@@ -23,7 +24,23 @@ test.describe("Test Different types of Dropdown ", () => {
     //Test Case Execution From Here....................................
 
     test("Select the apple using visible text", async () => {
-      await dropdownPage.selectFruit({label:"Orange"})
+      await dropdownPage.selectFruit(DropDownData.fruitName)
+      expect(await dropdownPage.getSelectedFruitName()).toContain(DropDownData.fruitName)
     })
+    test("Select your super hero's", async () => {
+      await dropdownPage.selectSuperHero(DropDownData.heroName,DropDownData.heroValue,DropDownData.heroIndex)
+      expect(await dropdownPage.getSelectedHeroName()).toContain(DropDownData.heroName)
+    })
+
+    test("Select the last programming language and print all the options", async () => {
+      const lang = await dropdownPage.getAllLangOptions()
+      expect(await dropdownPage.getSelectedProgrammingLanguage()).toContain(lang)
+    })
+
+    test("Select India using value & print the selected value", async () => {
+      const country = await dropdownPage.selectCounty(DropDownData.countryName)
+      expect(country).toBe(DropDownData.countryName)
+    })
+
 })
 
